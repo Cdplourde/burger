dff9e01a25180d8e6ccbbc176e9a19c264845dc8
cdplourde <cdplourde@gmail.com>
2018-05-03 19:46:38 -0700

    initial file structure and DB seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", false), ("Veggie (fake) burger", false), ("Mac & Cheese Burger", false);
+
+SELECT * FROM burgers;
